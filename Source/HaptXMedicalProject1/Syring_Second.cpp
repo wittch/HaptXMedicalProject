2c17b8690d4a980e581807b7a382257b9a8580e5
wittch <76587976+wittch@users.noreply.github.com>
2023-01-12 16:50:13 +0900

    syring BP -> cpp 변경 중
    Hx 1D Translator로 piston의 위치가 고정이 안되는 문제발견
    (추후 수정)
    Syring_Second.cpp를 상속받은 BP제작하여 mesh만 블루프린트로 넣어줌.
    나머지 함수는 제거

--- Source/HaptXMedicalProject1/Syring_Second.cpp
+++ Source/HaptXMedicalProject1/Syring_Second.cpp
@@ -6,14 +6,28 @@
 #include "Components/StaticMeshComponent.h"
 #include "Components/SceneComponent.h"
 #include "Math/TransformNonVectorized.h"
+#include "Medicine_Bottle_Second.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Engine/Engine.h"
+
 // Sets default values
 ASyring_Second::ASyring_Second():
-	Amount(0)
+	Amount(0),
+	IsOverlapped(false)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	Cylinder = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cylinder"));
+	Cylinder->SetupAttachment(RootComponent);
+	Cylinder->OnComponentBeginOverlap.AddDynamic(this, &ASyring_Second::OnOverlapBegin);
+	Cylinder->OnComponentEndOverlap.AddDynamic(this, &ASyring_Second::OnOverlapEnd);
 
-	
+
+	Piston = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Piston"));
+	Piston->SetupAttachment(Cylinder);
+
+	Medicine = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Medicine"));
+	Medicine->SetupAttachment(Cylinder);
 
 }
 
@@ -22,27 +36,73 @@ void ASyring_Second::BeginPlay()
 {
 	Super::BeginPlay();
 
+}
+
+void ASyring_Second::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
 	
+	AMedicine_Bottle_Second* castedActor{};
+	if (OtherActor ) 
+	{
+		castedActor = Cast<AMedicine_Bottle_Second>(OtherActor);
+	}
+		
+
+	if (castedActor)
+	{
+		IsOverlapped = true;
+	}
+}
+
+void ASyring_Second::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	AMedicine_Bottle_Second* castedActor{};
+	if ((OtherActor && (OtherActor != this) && OtherComp))
+	{
+		castedActor = Cast<AMedicine_Bottle_Second>(OtherActor);
+	}
+
+	if (castedActor)
+	{
+		IsOverlapped = false;
+	}
 }
 
 // Called every frame
 void ASyring_Second::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	if (IsOverlapped)
+	{
+		CalcMedicineAmount();
+	}
 }
 
-void ASyring_Second::CalcMedicineAmount(UStaticMeshComponent* mesh)
+void ASyring_Second::CalcMedicineAmount()
 {
-	Amount = mesh->RelativeLocation.Y * 0.1f;
-	FVector Meshoffset{ 0,Amount,0 };
-	
-	FTransform MeshTransform;
-	MeshTransform.SetLocation(mesh->GetComponentLocation());
-	MeshTransform.SetRotation(mesh->GetComponentRotation().Quaternion());
-	MeshTransform.SetScale3D(Meshoffset);
+	Amount = GetCylinderToPistonDistance();
+	Amount = Amount * 0.01f;
+	Amount = FMath::Clamp(Amount, 0.0f, 1.5f);
 
-	mesh->SetWorldTransform(MeshTransform);
-	
+	GEngine->AddOnScreenDebugMessage(-1, 30, FColor::Cyan, FString::Printf(TEXT("%f"), Amount));
+	FVector CylinderLocation = { 0.0f, -5.0f, 30.0f };
+	FVector CylinderScale = { 0.5f, 0.5f, Amount };
+	FTransform CylinderTransform;
+	CylinderTransform.SetLocation(CylinderLocation);
+	CylinderTransform.SetRotation(Medicine->GetRelativeTransform().GetRotation());
+	CylinderTransform.SetScale3D(CylinderScale);
+	Medicine->SetRelativeScale3D(CylinderScale);
+}
+
+float ASyring_Second::GetCylinderToPistonDistance()
+{
+
+	FVector CylinderLocation = Cylinder->GetComponentLocation();
+	FVector PistonLocation = Piston->GetComponentLocation();
+	FVector Coord = { CylinderLocation.X - PistonLocation.X, CylinderLocation.Y - PistonLocation.Y , CylinderLocation.Z - PistonLocation.Z };
+	float Distance = pow(Coord.X, 2) + pow(Coord.Y, 2) + pow(Coord.Z, 2);
+	Distance = sqrt(Distance);
+
+	return Distance;
 }
 
--- Source/HaptXMedicalProject1/Syring_Second.h
+++ Source/HaptXMedicalProject1/Syring_Second.h
@@ -18,15 +18,41 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 	
-	float Amount;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION(BlueprintCallable)
-	void CalcMedicineAmount(UStaticMeshComponent* mesh);
+	void CalcMedicineAmount();
+
+	UFUNCTION(BlueprintCallable)
+	float GetCylinderToPistonDistance();
+
 
 	FORCEINLINE float GetAmount() { return Amount; }
 
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Cylinder;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Piston;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Medicine;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool IsOverlapped;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Amount;
 };
